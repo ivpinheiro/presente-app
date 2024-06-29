abb59acea6e3a85ab98bc981b74ebc4677c80b6a
erika-hortencia <erika.cardoso@usp.br>
2024-06-29 19:57:19 -0300

    Minor changes in ProfileScreen.tsx confirm changes button added

--- components/ProfileScreen.tsx
+++ components/ProfileScreen.tsx
@@ -5,11 +5,13 @@ import {
   TextInput,
   TouchableOpacity,
   StyleSheet,
+  Dimensions,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { Formik } from "formik";
 import { useAuth } from "@/app/context/AuthContext";
+import api from "@/app/context/api";
 
 const ProfileScreen = () =>{
     const [email, setEmail] = useState('cora.almeida@gmail.com');
@@ -18,6 +20,38 @@ const ProfileScreen = () =>{
     const [isEmailEditable, setIsEmailEditable] = useState(false);
     const [isNameEditable, setIsNameEditable] = useState(false);
     const [isPasswordEditable, setIsPasswordEditable] = useState(false);
+    const [isButtonVisible, setIsButtonVisible] = useState(false);
+
+    const handleEmailChange = (newEmail: string) => {
+      setEmail(newEmail);
+      setIsButtonVisible(true);
+    };
+  
+    const handleNameChange = (newName: string) => {
+      setName(newName);
+      setIsButtonVisible(true);
+    };
+  
+    const handlePasswordChange = (newPassword: string) => {
+      setPassword(newPassword);
+      setIsButtonVisible(true);
+    };
+  
+    const handleConfirmChanges = () => {
+      // Substitui por chamada a API
+      console.log("Alterações confirmadas:");
+      console.log("Email:", email);
+      console.log("Nome:", name);
+      console.log("Senha:", password);
+  
+      // Atualizar estados para não editáveis
+      setIsEmailEditable(false);
+      setIsNameEditable(false);
+      setIsPasswordEditable(false);
+  
+      // Ocultar botão de confirmação
+      setIsButtonVisible(false);
+    };
 
     return (
       <View style={styles.container}>
@@ -31,47 +65,59 @@ const ProfileScreen = () =>{
           <FontAwesome5 name="user" size={50} color="black" />
         </View>
       </LinearGradient>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>e-mail</Text>
-        <TextInput
-          style={styles.input}
-          value={email}
-          onChangeText={setEmail}
-          editable={isEmailEditable}
-          placeholder="cora.almeida@gmail.com"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <View style={styles.row}>
-          <Text style={styles.label}>Nome</Text>
-          <TouchableOpacity onPress={() => setIsNameEditable(!isNameEditable)}>
-            <FontAwesome5 name="pen" size={15} color="black" style={styles.icon} />
-          </TouchableOpacity>
+      <View style={styles.contentContainer}>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>e-mail</Text>
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={handleEmailChange}
+            editable={isEmailEditable}
+            placeholder="cora.almeida@gmail.com"
+          />
         </View>
-        <TextInput
-          style={styles.input}
-          value={name}
-          onChangeText={setName}
-          editable={isNameEditable}
-          placeholder="Cora Almeida"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <View style={styles.row}>
-          <Text style={styles.label}>Senha</Text>
-          <TouchableOpacity onPress={() => setIsPasswordEditable(!isPasswordEditable)}>
-            <FontAwesome5 name="pen" size={15} color="black" style={styles.icon} />
-          </TouchableOpacity>
+        <View style={styles.inputContainer}>
+          <View style={styles.row}>
+            <Text style={styles.label}>Nome</Text>
+            <TouchableOpacity onPress={() => setIsNameEditable(!isNameEditable)}>
+              <FontAwesome5 name="pen" size={15} color="black" style={styles.icon} />
+            </TouchableOpacity>
+          </View>
+          <TextInput
+            style={styles.input}
+            value={name}
+            onChangeText={handleNameChange}
+            editable={isNameEditable}
+            placeholder="Cora Almeida"
+          />
         </View>
-        <TextInput
-          style={styles.input}
-          value={password}
-          onChangeText={setPassword}
-          editable={isPasswordEditable}
-          placeholder="*******"
-          secureTextEntry
-        />
+        <View style={styles.inputContainer}>
+          <View style={styles.row}>
+            <Text style={styles.label}>Senha</Text>
+            <TouchableOpacity onPress={() => setIsPasswordEditable(!isPasswordEditable)}>
+              <FontAwesome5 name="pen" size={15} color="black" style={styles.icon} />
+            </TouchableOpacity>
+          </View>
+          <TextInput
+            style={styles.input}
+            value={password}
+            onChangeText={handlePasswordChange}
+            editable={isPasswordEditable}
+            placeholder="*******"
+            secureTextEntry
+          />
+        </View>
       </View>
+      {isButtonVisible && (
+        <LinearGradient
+          colors={["#5859e9", "#52337c"]}
+          style={styles.confirmButton}
+        >
+          <TouchableOpacity style={styles.confirmButtonInner} onPress={handleConfirmChanges}>
+            <Text style={styles.confirmButtonText}>Confirmar Alterações</Text>
+          </TouchableOpacity>
+        </LinearGradient>
+      )}
     </View>
       );
 }
@@ -80,7 +126,7 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       justifyContent: "center",
-      padding: 40,
+      padding: 0,
       gap: 60,
     },
     label: {
@@ -103,9 +149,9 @@ const styles = StyleSheet.create({
       borderColor: "gray",
       borderWidth: 1,
       paddingLeft: 10,
+      
     },
     banner: {
-      position: 'absolute',
       top: 0,
       left: 0,
       right: 0,
@@ -120,8 +166,8 @@ const styles = StyleSheet.create({
         left: '50%',
         width: 120,
         height: 120,
-        marginTop: -20, // Metade da altura para centralizar verticalmente
-        marginLeft: -60, // Metade da largura para centralizar horizontalmente
+        marginTop: -20, 
+        marginLeft: -60,
         borderRadius: 60,
         backgroundColor: '#fff',
         justifyContent: 'center',
@@ -131,7 +177,7 @@ const styles = StyleSheet.create({
       profileImage: {
         width: '100%',
         height: '100%',
-        borderRadius: 60, // Para tornar a imagem circular
+        borderRadius: 60,
       },
     containerLogo: {
       flexDirection: "row",
@@ -170,6 +216,30 @@ const styles = StyleSheet.create({
     },
     icon: {
       marginRight: 8,
+    },
+    confirmButton: {
+      position: 'absolute',
+      bottom: 20,
+      left: 40,
+      right: 40,
+      padding: 2,
+      borderRadius: 10,
+      overflow: "hidden",
+    },
+    confirmButtonInner: {
+      alignItems: 'center',
+      padding: 15,
+    },
+    confirmButtonText: {
+      color: "#fff",
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+    contentContainer: {
+      flex: 1,
+      justifyContent: "center",
+      padding: 40,
+      gap: 60,
     },
   });
   
